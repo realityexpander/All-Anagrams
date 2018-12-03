274b2d3cf8bc34cb0e5ed3320e66c5d1f4dd146e
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-03 09:35:47 -0600

    Update allAnagrams.js

--- allAnagrams.js
+++ allAnagrams.js
@@ -1,47 +1,20 @@
 
-function allAnagrams (string) {
-  let len = string.length;
-  let res = [];
 
-  function permutations(str) {
-    if(str.length === 1) {
-      res.push([string]);
-    } else {
-      permutations(str.slice(1));
-      for(let j=0; j<str.length-1; j++) {
-        let c=str[j];
-        str[j]=str[i];
-        str[i]=c;
-      }
-    }
-  }
-
+function allAnagrams(str, index=0, storage=[]) {
+    if (typeof str == "string")
+        str = str.split("");
+    if (index >= str.length)
+        return storage;
+    for (var i = index; i < str.length; i++)
+        storage.push(ToggleLetters(str, index, i));
+    return allAnagrams(str, index + 1, storage);
 }
 
-
-// 100 PROGRAM "Permutat.bas"
-// 110 LET N=4 // Number of elements
-// 120 NUMERIC T(1 TO N)
-// 130 FOR I=1 TO N
-// 140   LET T(I)=I
-// 150 NEXT
-// 160 LET S=0
-// 170 CALL PERM(N)
-// 180 PRINT "Number of permutations:";S
-// 190 END
-// 200 DEF PERM(I)
-// 210   NUMERIC J,X
-// 220   IF I=1 THEN
-// 230     FOR X=1 TO N
-// 240       PRINT T(X);
-// 250     NEXT 
-// 260     PRINT :LET S=S+1
-// 270   ELSE
-// 280     CALL PERM(I-1)
-// 290     FOR J=1 TO I-1
-// 300       LET C=T(J):LET T(J)=T(I):LET T(I)=C
-// 310       CALL PERM(I-1)
-// 320       LET C=T(J):LET T(J)=T(I):LET T(I)=C
-// 330     NEXT
-// 340   END IF
-// 350 END DEF
+function ToggleLetters(str, index1, index2) {
+    if (index1 != index2) {
+        var temp = str[index1];
+        str[index1] = str[index2];
+        str[index2] = temp;
+    }
+    return str.join("");
+}
