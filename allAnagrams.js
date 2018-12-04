7a36d44fd8b65d7d64f20daef00ae2bcb34ecf97
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-03 20:53:27 -0600

    Update allAnagrams.js

--- allAnagrams.js
+++ allAnagrams.js
@@ -1,20 +1,21 @@
+console.log(allAnagrams("abc"));
 
+//string permutation
+function allAnagrams(string, start='') {
 
-function allAnagrams(str, index=0, storage=[]) {
-    if (typeof str == "string")
-        str = str.split("");
-    if (index >= str.length)
-        return storage;
-    for (var i = index; i < str.length; i++)
-        storage.push(ToggleLetters(str, index, i));
-    return allAnagrams(str, index + 1, storage);
-}
-
-function ToggleLetters(str, index1, index2) {
-    if (index1 != index2) {
-        var temp = str[index1];
-        str[index1] = str[index2];
-        str[index2] = temp;
+  if (string.length == 1) {
+    return [start + string];
+    // base
+  } else {
+    var res = [];
+    for (var i = 0; i < string.length; i++) {
+      var result = allAnagrams(string.substr(0, i) + string.substr(i + 1), string[i]);
+      for (var j = 0; j < result.length; j++) {
+        let val = start + result[j];
+        if(res.indexOf(val) < 0)
+          res.push(val);
+      }
     }
-    return str.join("");
+    return res;
+  }
 }
